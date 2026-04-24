4e884f04626e1bfc48a17c68ee7d866f5e8c1e38
luismiquel <luismiguelgarciadelasmorenas@gmail.com>
2026-04-24 21:06:55 +0200

    fix definitivo limpiar service worker cache

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,20 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => registration.unregister());
+  });
+
+  if (window.caches) {
+    caches.keys().then((keys) => {
+      keys.forEach((key) => caches.delete(key));
+    });
+  }
+}
+
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-  
