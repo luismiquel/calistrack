b765beb6f17a988ba2cd5d8931f210d4d9fe532e
luismiquel <luismiguelgarciadelasmorenas@gmail.com>
2026-04-24 20:45:16 +0200

    fix: remove service worker (white screen fix)

--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,4 @@ createRoot(document.getElementById("root")).render(
   </React.StrictMode>
 );
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js");
-  });
-}
+  
