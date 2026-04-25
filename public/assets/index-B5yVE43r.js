331ab9ad13e3060131e6694587f6dffc7eb4a827
luismiquel <luismiguelgarciadelasmorenas@gmail.com>
2026-04-25 18:32:16 +0200

    fix old cached asset redirect

---
+++ public/assets/index-B5yVE43r.js
@@ -0,0 +1,21 @@
+﻿(function () {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.getRegistrations().then(function (regs) {
+      regs.forEach(function (reg) {
+        reg.unregister();
+      });
+    });
+  }
+
+  if (window.caches) {
+    caches.keys().then(function (keys) {
+      keys.forEach(function (key) {
+        caches.delete(key);
+      });
+    });
+  }
+
+  setTimeout(function () {
+    window.location.href = "/?fixed=1";
+  }, 800);
+})();
